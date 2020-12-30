dee6d03807dc525f510f38aaa6e40d4b56c26ae4
DawiT <Dawid@Tararuj.be>
2020-12-30 14:50:21 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+let tableau = ['coding 15', 'coding 16', 'coding 17'];
+
+
+tableau.forEach(function (cont){ console.log(cont) }); 
+
+tableau.push("dawiteu");
+
+
+tableau.forEach(function (cont){ console.log(cont) }); 
